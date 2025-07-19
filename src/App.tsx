58161386203803b7dfa86fa045d781c2908b2933
Ahmad-HB <ahmadhbahbeh2004@gmail.com>
2025-07-19 15:45:02 +0300

    complete top controls tabs

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,21 @@ import './index.css';
 function App() {
   return (
     <div className="flex flex-col h-[100%] w-[100%]">
-      <div className="h-[10%] border-black-50 border-2">
+      <div className="h-[10%] ">
         <TopControls />
       </div>
-      <div className="flex flex-row flex-grow">
-      <div className="w-[20%] border-black-50 border-2">
+      <div className="flex flex-row flex-grow gap-1">
+      <div className="w-[15%] border-[#969696] border-2 rounded-r-lg">
         <Sidebar />
       </div>
-      <div className="w-[80%] border-black-50 border-2">
+      <div className="w-[85%] border-[#969696] border-10 border-dashed rounded-lg ">
         <MainView />
         <div className="content-section">
 
         </div>
       </div>
       </div>
-      <div className="flex h-[10%] justify-center items-center border-black-50 border-2">
+      <div className="flex h-[10%] justify-center items-center">
         <BottomPanel />
       </div>
     </div>
--- src/Components/TopControls.tsx
+++ src/Components/TopControls.tsx
@@ -4,10 +4,10 @@ import React, { useState } from "react";
 import { HeroUIProvider } from "@heroui/react";
 
 import {
-Database,
-Cpu,
-Circuitry,
-Binary
+  Database,
+  Cpu,
+  Circuitry,
+  Binary
 } from "@phosphor-icons/react";
 
 
@@ -39,44 +39,41 @@ export const BinaryIcon = () => {
 
 
 
-
-
-
 export default function TopControls() {
   const [selected, setSelected] = React.useState("photos");
 
 
   return (
     <div className="flex justify-center items-center w-full h-full">
-    <HeroUIProvider>
-      <Tabs color="default" fullWidth className='flex' aria-label="Options" variant="bordered" selectedKey={selected} onSelectionChange={(key) => setSelected(String(key))}>
-        <Tab key="Data-Structures" title={
-          <div className="flex items-center space-x-2 w-full h-full">
-            <DatabaseIcon />
-            <span>Data Structures</span>
-          </div>
-        } />
-        <Tab key="OS-Algorithms" title={
-          <div className="flex items-center space-x-2 w-full h-full">
-            <CpuIcon />
-            <span>OS Algorithms</span>
-          </div>
-        } />
-        <Tab key="Logic Gates" title={
-          <div className="flex items-center space-x-2 w-full h-full">
-            <CircuitryIcon />
-            <span>Logic Gates</span>
-          </div>
-        } />
-        <Tab key="Numbering-System" title={
-          <div className="flex items-center space-x-2 w-full h-full">
-          <BinaryIcon />
-          <span>Numbering system</span>
-          </div>
-        } />
+      <HeroUIProvider>
+        <Tabs fullWidth aria-label="Options" variant="bordered" selectedKey={selected} onSelectionChange={(key) => setSelected(String(key))}>
+          <Tab key="Data-Structures" title={
+            <div className="flex items-center space-x-2 w-full h-full">
+              <DatabaseIcon />
+              <span>Data Structures</span>
+            </div>
+          } />
+          <Tab key="OS-Algorithms" title={
+            <div className="flex items-center space-x-2 w-full h-full">
+              <CpuIcon />
+              <span>OS Algorithms</span>
+            </div>
+          } />
+          <Tab key="Logic Gates" title={
+            <div className="flex items-center space-x-2 w-full h-full">
+              <CircuitryIcon />
+              <span>Logic Gates</span>
+            </div>
+          } />
+          <Tab key="Numbering-System" title={
+            <div className="flex items-center space-x-2 w-full h-full">
+              <BinaryIcon />
+              <span>Numbering system</span>
+            </div>
+          } />
 
-      </Tabs>
-    </HeroUIProvider>
+        </Tabs>
+      </HeroUIProvider>
     </div>
   )
 }
